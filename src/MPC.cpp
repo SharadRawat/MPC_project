56cc1738b2f6b24986bda369886c39692b8d782f
Sharad Rawat <rawat.33@osu.edu>
2018-10-04 10:02:21 -0400

    Account for latency

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -90,6 +90,10 @@ for (int t = 1; t < N; t++) {
   // Only consider the actuation at time t.
   AD<double> delta0 = vars[delta_start + t - 1];
   AD<double> a0 = vars[a_start + t - 1];
+  if (t > 1) {   // use previous actuations (to account for latency)
+        a0 = vars[a_start + t - 2];
+        delta0 = vars[delta_start + t - 2];
+      }
 
   AD<double> f0 = coeffs[0] + coeffs[1] * x0;
   AD<double> psides0 = CppAD::atan(coeffs[1]);
